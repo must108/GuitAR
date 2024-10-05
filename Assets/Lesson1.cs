a4afbb805044372b1ba36d542ea02d9c313b1e97
must108 <136145181+must108@users.noreply.github.com>
2024-10-05 19:00:31 -0400

    add lesson class

--- Assets/Lesson1.cs
+++ Assets/Lesson1.cs
@@ -1,44 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using System;
-using UnityEngine.XR.Interaction.Toolkit;
 
-public class Lesson1 : MonoBehaviour {
-    // static System.Random random = new System.Random();
-    // public static int[,] ReturnRandomNote() {
-    //     int[,] ReturnArray = new int[4,6];
-
-    //     for (int i = 0; i < ReturnArray.GetLength(0); i++) {
-    //         for (int j = 0; j < ReturnArray.GetLength(1); j++) {
-    //             ReturnArray[i, j] = random.Next(0, 2);
-    //         }
-    //     }
-
-    //     return ReturnArray;
-    // }
-
-    public void LoadFretPositions(int[,] fretArray) {
-        // Loop through all children of this GameObject
-        foreach (Transform child in transform) {
-            // Access the child GameObject
-            FretSphere fretSphere = child.gameObject.GetComponent<FretSphere>();
-            if (fretSphere == null) continue; // Safeguard against missing component
-            
-            int rowIndex = fretSphere.sphereRowIndex;
-            int colIndex = fretSphere.sphereColIndex;
-            if (rowIndex < fretArray.GetLength(0) && colIndex < fretArray.GetLength(1)) {
-                child.gameObject.SetActive(fretArray[rowIndex, colIndex] == 1);
-            }
-        }
-    }
-
-    // public static string ReturnRandomNoteName() {
-    //     string[] noteNames = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
-    //     return noteNames[random.Next(0, noteNames.Length)];
-    // }
-
+public class Lesson1 : Lesson {
     private int[,] SAMPLE_OBJECT;
     private string SAMPLE_NOTE;
 
