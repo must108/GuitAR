e80af67d3c77dc06eb33792cc12ce6c48b7a9804
Jordan Muniz <jordy.valois@gmail.com>
2024-10-06 03:10:18 -0400

    Loading Lessons

--- Assets/LessonManager.cs
+++ Assets/LessonManager.cs
@@ -2,42 +2,59 @@ using System.Threading;
 using UnityEngine;
 using UnityEngine.Video;
 
-public class LessonManager : MonoBehaviour {
-    public Music currentLesson;
-    public string[] currentNotes;
+public class LessonManager : MonoBehaviour
+{
+    private Music currentLesson;
+    private string[] currentNotes;
+    private bool abort = false;
+    [SerializeField] private GameObject guitarObjectSpheres;
     private int count = 0;
     private float timer = 0f;
-
-    public void LoadFretPositions(int[,] fretArray) {
+    public void Abort()
+    {
+        abort = true;
+    }
+    public void LoadFretPositions(int[,] fretArray)
+    {
         // Loop through all children of this GameObject
-        foreach (Transform child in transform) {
+        foreach (Transform child in guitarObjectSpheres.transform)
+        {
             // Access the child GameObject
             FretSphere fretSphere = child.gameObject.GetComponent<FretSphere>();
             if (fretSphere == null) continue; // Safeguard against missing component
-            
+
             int rowIndex = fretSphere.sphereRowIndex;
             int colIndex = fretSphere.sphereColIndex;
-            if (rowIndex < fretArray.GetLength(0) && colIndex < fretArray.GetLength(1)) {
+            if (rowIndex < fretArray.GetLength(0) && colIndex < fretArray.GetLength(1))
+            {
                 child.gameObject.SetActive(fretArray[rowIndex, colIndex] == 1);
             }
         }
     }
 
-    public void LoadLesson(Music lesson) {
+    public void LoadLesson(Music lesson)
+    {
         currentLesson = lesson;
         currentNotes = currentLesson.GetNotes();
         count = 0;
         LoadFretPositions(Music.NoteObjects[currentNotes[count]]);
     }
 
-    void Start() {
-        LoadLesson(new Lesson2());
-    }
+    void Update()
+    {
+        if (abort)
+        {
+            abort = false;
+            LoadFretPositions(new int[,]{
+                { 0, 0, 0, 0, 0, 0}, { 0, 0, 0, 0, 0, 0}, { 0, 0, 0, 0, 0, 0},{ 0, 0, 0, 0, 0, 0},
+            });
 
-    void Update() {
-        if (count < currentNotes.Length) {
+        }
+        if (count < currentNotes.Length)
+        {
             timer += Time.deltaTime;
-            if (timer >= Music.GetInterval()) {
+            if (timer >= Music.GetInterval())
+            {
                 count += 1;
                 LoadFretPositions(Music.NoteObjects[currentNotes[count]]);
                 timer = 0f;
--- Assets/SimpleLessonManager.cs
+++ Assets/SimpleLessonManager.cs
@@ -7,41 +7,47 @@ public class SimpleLessonManager : MonoBehaviour
     [SerializeField] private GameObject subLessons;
     [SerializeField] private GameObject exitButton;
     [SerializeField] private GameObject goBackButton;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Music lesson1;
+    [SerializeField] private Music lesson2;
+    [SerializeField] private Music lesson3;
+    [SerializeField] private Music lesson4;
+
+    [SerializeField] private LessonManager lessonManager;
 
     public void StartLessonEddie()
     {
         subLessons.SetActive(false);
         goBackButton.SetActive(false);
         exitButton.SetActive(true);
+        lessonManager.LoadLesson(lesson1);
     }
     public void StartLessonGood()
     {
         subLessons.SetActive(false);
         goBackButton.SetActive(false);
         exitButton.SetActive(true);
+        lessonManager.LoadLesson(lesson2);
     }
     public void StartLessonCMajor()
     {
         subLessons.SetActive(false);
         goBackButton.SetActive(false);
         exitButton.SetActive(true);
+        lessonManager.LoadLesson(lesson3);
     }
     public void StartLessonGmajor()
     {
         subLessons.SetActive(false);
         goBackButton.SetActive(false);
         exitButton.SetActive(true);
+        lessonManager.LoadLesson(lesson4);
     }
     public void ExitSubLesson()
     {
         subLessons.SetActive(true);
         exitButton.SetActive(false);
         goBackButton.SetActive(true);
+        lessonManager.Abort();
     }
 
 }
